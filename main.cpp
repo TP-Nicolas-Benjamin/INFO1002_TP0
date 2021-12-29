edde3a862fe58772404aef1ee1ab2a59192fc75c
Commandeur Nicolas <comcomnico@gmail.com>
2021-12-29 19:41:53 +0100

    structure changement + Q3.1 done

--- main.cpp
+++ main.cpp
@@ -13,14 +13,14 @@ using namespace std;
 map<string, double> log_tetragrammes {};
 int total_tetragrammes = 0;
 
-char alphabet[] = {
-    'A', 'B', 'C', 'D', 'E',
-    'F', 'G', 'H', 'I', 'K',
-    'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U',
-    'V', 'W', 'X', 'Y', 'Z'
+vector<vector<char>> alphabet = {
+    {'A', 'B', 'C', 'D', 'E'},
+    {'F', 'G', 'H', 'I', 'K'},
+    {'L', 'M', 'N', 'O', 'P'},
+    {'Q', 'R', 'S', 'T', 'U'},
+    {'V', 'W', 'X', 'Y', 'Z'}
 };
-vector<char> key = {};
+vector<vector<char>> key = {{}, {}, {}, {}, {}};
 
 vector<string> split (const string &s, char delim) {
     vector<string> result;
@@ -79,34 +79,141 @@ double score(string text) {
     return score;
 }
 
-vector<char> genere_cle(string texte) {
-    vector<char> k = {};
-    for (char& c : texte) {
-        if (find(k.begin(), k.end(), c) == k.end()) {
-            if (c == 'J') k.push_back('I');
-            else k.push_back(c);
+bool key_contains_char(vector<vector<char>> k, char c) {
+    for (vector<char> v : k ) {
+        if (find(v.begin(), v.end(), c) != v.end()) {
+            return true;
         }
     }
+    return false;
+}
 
-    for (char& c : alphabet) {
-        if (find(k.begin(), k.end(), c) == k.end()) {
-            k.push_back(c);
+vector<vector<char>> genere_cle(string texte) {
+    vector<vector<char>> k = {{}, {}, {}, {}, {}};
+    if (texte == "") {
+        k = alphabet;
+    } else {
+        int last_y = 0;
+        int last_x = 0;
+
+        int txtLenght = texte.length();
+        int kSize = k.size();
+
+        for (
+            int y = 0;
+            y < (
+                txtLenght < kSize * kSize 
+                ? ((int) txtLenght / kSize) + 1
+                : kSize
+            );
+            y++)
+        {
+            for (char& c : texte.substr(y*5, txtLenght)) {
+                if (!(key_contains_char(k, c))) {
+                    if (c == 'J') k[y].push_back('I');
+                    if (c == 'W') k[y].push_back('V');
+                    else k[y].push_back(c);
+                    last_x = (last_x + 1) % 5;
+                    if (last_x == 0) {
+                        last_y = (last_y + 1) % 5;
+                    }
+                }
+                if (k[y].size() == 5) break;
+            } 
+        }
+
+        cout << "last y : " << last_y << endl;
+        cout << "last x : " << last_x << endl;
+
+        for (vector<char> v : alphabet) {
+            if (last_x == 5) {
+                last_y++;
+            }
+            for (char& c : v) {
+                if (!key_contains_char(k, c)) {
+                    k[last_y].push_back(c);
+                    last_x = (last_x + 1) % 5;
+                    if (last_x == 0) {
+                        last_y = (last_y + 1) % 5;
+                    }
+                }
+            }
         }
     }
 
     return k;
 }
 
-void affiche_cle(vector<char> k) {
+void affiche_cle(vector<vector<char>> k) {
     int counter = 0;
 
     cout << "Current key is : " << endl;
-    for (char& c : k) {
-        counter++;
-        cout << c << " ";
-        if (counter % 5 == 0) {
-            cout << endl;
+    for (vector<char> v : k) {
+        for (char& c : v) {
+            cout << c << " ";
         }
+        cout << endl;
+    }
+}
+
+pair<int, int> coords_of_char(vector<vector<char>> k, char c) {
+    for (int y = 0; y < k.size(); y++) {
+        for (int x = 0; x < k[y].size(); x++) {
+            if (k[y][x] == c) return make_pair(y,x);
+        }
+    }
+    cout << c << " not found !" << endl;
+    return make_pair(-1,-1);
+}
+
+pair<char, char> chiffre_pair(vector<vector<char>> k, char a, char b) {
+    int xa = -1;
+    int xb = -1;
+    int ya = -1;
+    int yb = -1;
+
+    pair<int, int> coords;
+    for (vector<char> v : k)  {
+        for (char& c : v) {
+            if (c == a) {
+                coords = coords_of_char(k, c);
+                cout << (coords.first != -1 && coords.second != -1) << endl;
+                if (coords.first != -1 && coords.second != -1) {
+                    ya = coords.first;
+                    xa = coords.second;
+                }
+            }
+            if (c == b) {
+                coords = coords_of_char(k, c);
+                cout << (coords.first != -1 && coords.second != -1) << endl;
+                if (coords.first != -1 && coords.second != -1) {
+                    cout << "not -1" << endl;
+                    yb = coords.first;
+                    xb = coords.second;
+                }
+            }
+            if (xa != -1 && xb != -1 && ya != -1 && yb != -1) break;
+        }
+    }
+
+    cout << xa << "\t" << xb << endl;
+    cout << ya << "\t" << yb << endl;
+    if (xa != -1 && xb != -1 && ya != -1 && yb != -1) {
+        pair<char, char> res;
+
+        if (xa == xb) {
+            cout << "on same line" << endl;
+        } else if (ya == yb) {
+            cout << "on same column" << endl;
+        } else {
+            cout << "different column and line" << endl;
+            res.first = k[ya][xb];
+            res.second = k[yb][xa];
+        }
+        return res;
+    } else {
+        cout << "Une des lettres n'est pas disponible dans la clé !";
+        return make_pair(a,b);
     }
 }
 
@@ -127,6 +234,16 @@ int main(int argc, char const *argv[])
 
     key = genere_cle(keyword_gen);
     affiche_cle(key);
+
+    // Avec 2 lettres trouvées
+    cout << endl << "I\tK" << endl;
+    auto p = chiffre_pair(key, 'I', 'K');
+    cout << "I -> " << p.first << "\t" << "K -> " << p.second << endl;
+
+    // Avec 1 lettre qui n'existe pas (J)
+    cout << endl << "M\tJ" << endl;
+    p = chiffre_pair(key, 'M', 'J');
+    cout << "M -> " << p.first << "\t" << "J -> " << p.second << endl;
 
     return 0;
 }
